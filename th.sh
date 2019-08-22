eda3b6c25140f145761ab05756f411b4bd0936c9
Petr Machata <petrm@mellanox.com>
2019-08-22 18:10:21 +0300

    th.sh: Introduce gauges with baseline and other changes
    Change the way the script decides when to update baseline. Previously it
    was either ever tick or never. When never was selected, the units were
    always bytes.
    
    Instead decouple the two: make it possible to have gauges that measure
    something else than bytes. B:, P: and uS: still select units. G: was
    dropped, and instead N: was introduce to choose to update never. B: N: is
    exactly what G: used to be.
    
    Additionally, introduce a toggle T:, which selects update each tick (which
    was previously implied in all modes except G:); and O:, which updates
    baseline once, and then never again (so that one gets counter value
    relative to the time that the tool was started). Also introduce 1: to just
    show unitless value.
    
    Change B: to always mean bytes, introduce Bb: to mean bytes that should be
    shown as bits.
    
    Signed-off-by: Petr Machata <petrm@mellanox.com>

--- th.sh
+++ th.sh
@@ -1,40 +1,68 @@
 if [[ $# -lt 2 ]]; then
-	echo "Usage: $(basename $0) <if> <counter> <counter> ... <if2> ..."
-	echo " counters measures traffic in bytes, use B: to toggle"
-	echo " packet counters measures traffic in packets, use P: to toggle"
-	echo " us counters measures traffic in microseconds use uS: to toggle"
-	echo " gauges measure usage in bytes, use G: to toggle"
-	echo " default type is byte counters"
-	echo " default sleep between measurements is 1s. Use -s <time> to change"
-	echo
-	echo " by default, <counter> is an ethtool counter, but it can be prefixed"
-	echo "  with FAMILY/ like so:"
+	echo "Usage: $(basename $0) {<interface>|[<family>/]<counter>|<flags>}"
+        echo " - counter is a name of a counter that should be shown"
+        echo " - family is what type of counter it is. Possible families are:"
 	echo "    - ETH/name -- name of the counter in ethtool output"
+        echo "      this is the default"
 	echo "    - IPL/jqpath -- path inside stats64 object to get a counter"
 	echo "      in the output of ip -s -j link show dev <if>"
         echo "    - ING/jqpath -- path inside stats object to get a counter"
 	echo "      in the output of tc -s -j flow show dev <if> ingress"
         echo "    - EGR/jqpath -- likewise for egress"
+        echo
+        echo " - flags change the way that following counters will be shown"
+        echo "   P: the counter is in packets (that's the default)"
+        echo "   B: the counter is in bytes"
+        echo "   1: the counter is unitless"
+        echo "   Bb: the counter is bytes, but show it in bits"
+        echo "   uS: the counter is in microseconds"
+        echo
+        echo "   N: the value is shown as-is"
+        echo "   T: counter baseline value is updated on every tick"
+        echo "      and the shown value is per second increment"
+        echo "   O: counter baseline value is updated once when the"
+        echo "      tool is started. Thus the value shown is how much"
+        echo "      has the counter increased since the tool was started"
+        echo
+        echo "   -s <time> changes the amount of sleep between ticks"
+        echo "             The default is 1s"
 	echo
-	echo "e.g.: $(basename $0) sw1p6 rx_octets_prio_1 sw1p7 rx_octets_prio_2 sw1p10 rx_octets_prio_1 rx_octets_prio_2 G: sw1p9 tc_transmit_queue_tc_1 tc_transmit_queue_tc_2 IPL/tx.bytes ING/actions[0].stats.bytes"
+	echo "e.g.: $(basename $0) sw1p6 rx_octets_prio_1 sw1p7 rx_octets_prio_2 sw1p10 rx_octets_prio_1 rx_octets_prio_2 B: N: sw1p9 tc_transmit_queue_tc_1 tc_transmit_queue_tc_2 T: IPL/tx.bytes ING/actions[0].stats.bytes"
 	exit 1
 fi
 
 declare -a COUNTERS
 
-type=B
+type=P
+update=tick
 sleep=1
+kind=relative
 if=
 while [[ $# -gt 0 ]]; do
     arg=$1; shift
-    if [[ $arg == "G:" ]]; then
-	type=G
+    if [[ $arg == "N:" ]]; then
+	update=never
+        kind=absolute
+	continue
+    elif [[ $arg == "O:" ]]; then
+	update=once
+        kind=absolute
+	continue
+    elif [[ $arg == "T:" ]]; then
+	update=tick
+        kind=relative
+	continue
+    elif [[ $arg == "Bb:" ]]; then
+	type=Bb
 	continue
     elif [[ $arg == "B:" ]]; then
 	type=B
 	continue
     elif [[ $arg == "P:" ]]; then
 	type=P
+	continue
+    elif [[ $arg == "1:" ]]; then
+	type=1
 	continue
     elif [[ $arg == "uS:" ]]; then
 	type=uS
@@ -52,7 +80,8 @@ while [[ $# -gt 0 ]]; do
     fi
 
     counter=$arg
-    COUNTERS[${#COUNTERS[@]}]="type=$type if=$if counter=$arg"
+    COUNTERS[${#COUNTERS[@]}]="type=$type update=$update \
+                               kind=$kind if=$if counter=$arg"
 done
 
 humanize()
@@ -98,7 +127,9 @@ collect()
 	local ethout
 
 	for ((i=0; i< ${#COUNTERS[@]}; ++i)); do
-		eval ${COUNTERS[$i]}
+                local orig=
+                local val=
+                eval ${COUNTERS[$i]}
 		eval ${VALS[$i]}
 		if [[ $if != $last_if ]]; then
 			case "$counter" in
@@ -123,7 +154,11 @@ collect()
 				;;
 			esac
 		fi
-		orig=$((val))
+
+                if [[ $update == tick ]]; then
+                    orig=$((val))
+                fi
+
 		case "$counter" in
 			IPL/*)
 				val=$(($(echo "$ethout" | \
@@ -138,7 +173,12 @@ collect()
 					| head -n 1 | cut -d: -f2)))
 				;;
 		esac
-		VALS[$i]="orig=$orig val=$val"
+
+                if [[ $update == once && $orig == "" ]]; then
+                    orig=$((val))
+                fi
+
+                VALS[$i]="orig=$orig val=$val"
 	done
 }
 
@@ -157,22 +197,28 @@ while true; do
 	for ((i=0; i< ${#COUNTERS[@]}; ++i)); do
 	    eval ${COUNTERS[$i]}
 	    eval ${VALS[$i]}
-	    if [[ $type == B ]]; then
-		    val=$((8 * val))
-		    orig=$((8 * orig))
-		    rate=$(rate $orig $val $interval)
-		    echo -e "$if $counter\t\033[K$(humanize $rate bps)"
-	    elif [[ $type == P ]]; then
-		    rate=$(rate $orig $val $interval)
-		    echo -e "$if $counter\t\033[K$(humanize $rate pps)"
-	    elif [[ $type == uS ]]; then
-		    rate=$(rate $orig $val $interval)
-		    echo -e "$if $counter\t\033[K$(humanize $rate s/s u m)"
-	    elif [[ $type == G ]]; then
-		    echo -e "$if $counter\t\033[K$(humanize $val B)"
-	    else
-		    echo -e "$if $counter\t\033[Ktype=$type???"
-	    fi
+
+	    if [[ $type == Bb ]]; then
+                val=$((8 * val))
+                orig=$((8 * orig))
+            fi
+
+            if [[ $kind == relative ]]; then
+                show=$(rate $orig $val $interval)
+                unit=/s
+            else
+                show=$((val - orig))
+                unit=
+            fi
+
+            case $type in
+                Bb) unit="b$unit";;
+                B)  unit="B$unit";;
+                P)  unit="p$unit";;
+                uS) unit="s$unit u m";;
+            esac
+
+            echo -e "$if $counter\t\033[K$(humanize $show $unit)"
 	    N=$((N + 1))
 	done
 
